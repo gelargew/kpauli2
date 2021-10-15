b4ed3c5a0acde234dd81d3ec3685018670cb98ba
unknown <gelargew@gmail.com>
2021-10-15 10:09:44 +0700

    init

--- src/pages/kpauli.tsx
+++ src/pages/kpauli.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import Numpad from '../components/Numpad'
 import '../styles/kpauli.css'
 
@@ -7,7 +7,7 @@ const NUMBERS = new Array(5000).fill(0)
 
 export default function Kpauli() {
     const d = useRef<HTMLDivElement>()
-
+    const kp = useRef<HTMLDivElement>()
     const scaleNumbers = (e:React.WheelEvent) => {
         console.log(e)
     }
@@ -18,25 +18,29 @@ export default function Kpauli() {
         
         const y = e.clientY - e.currentTarget.getBoundingClientRect().top;  //y position within the element.
         console.log(x, y)
-        d.current.style.top = y+'px'
-        d.current.style.left = x+'px'
+ 
     }
+
+    useLayoutEffect(() => {
+        const rect = kp.current.getBoundingClientRect()
+        kp.current.scrollBy(-5000, 0)
+    }, [])
 
     return (
        <main>
            <section id='numbers' >
-               <div className='numbers' onWheel={scaleNumbers} >
-                   <div>123456789</div>
+                <div className='the-input' ref={d}></div>
+               <div ref={kp} className='numbers' onWheel={scaleNumbers} >
+                    
                    {new Array(50).fill(0).map((num, i) =>
                         <React.Fragment key={i}>
                             <div>{new Array(100).fill(i%10).join('')}</div>
                             <div className='numbers-inputs' onClick={getXY} >
-                                {new Array(99).fill(i%10).join('')}
+                                {' '.repeat(99)}
                             </div>
                         </React.Fragment> 
                         
                          )}
-                    <div className='the-input' ref={d}>HEL</div>
                </div>
             </section>
            <section>
